16af535a0bc1cf6a0cf6daa20cd901280c1d4813
Ayehubirhan Teshome Bogale <106815138+Ayehubirhan19@users.noreply.github.com>
2025-09-01 15:49:32 +0300

    Update file_handling.py

--- file_handling.py
+++ file_handling.py
@@ -20,12 +20,3 @@ def modify_file(input_file, output_file):
         print(f"Error: The file '{input_file}' does not exist.")
     except Exception as e:
         print(f"An error occurred: {e}")
-
-
-# Main program
-if __name__ == "__main__":
-    # Ask the user for the filename
-    input_file = input("Enter the name of the file to read: ").strip()
-    output_file = "modified_" + input_file  # create new file with prefix
-
-    modify_file(input_file, output_file)
